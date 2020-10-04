2eba1147f76f6b0a8bd806639bd3f7a393a37583
segundaetapa <50712691+segundaetapa@users.noreply.github.com>
2020-10-04 11:23:19 +0700

    Create SortingTime.cpp

---
+++ SortingTime.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp) { 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+
+int main() {
+	
+	int a[100], b[100], c[100], d[100];
+	
+	int n; 
+	
+	scanf("%d", &n)
+	
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d %d", &a[i], &b[i], &c[i]);
+		d[i] = a[i] * 3600 + b[i] * 60 + c[i];
+	}
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (d[i] > d[j]) {
+				swap(&a[i], &a[j]);
+				swap(&b[i], &b[j]);
+				swap(&c[i], &c[j]);
+			}
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d\n", a[i], b[i], c[i]);
+	} 
+	
+	return 0;
+}
