32980a5ebcccc9ba44c310615410d2b20f2615cd
blackjackals <65567021+blackjackals@users.noreply.github.com>
2020-10-04 11:48:49 +0700

    Update Array3D.cpp
    Add 3D Array

--- Array3D.cpp
+++ Array3D.cpp
@@ -3,7 +3,23 @@
 // Please Add an 3D Array
 int main() {
 
-
+	int x[3][3][3];
+	
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			for(int k=0; k<3; k++) {
+				scanf("%d", &x[i][j][k]);
+			}
+		}
+	}
+	
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			for(int k=0; k<3; k++) {
+				printf("Test[%d][%d][%d] = %d\n", i, j, k, x[i][j][k]);
+			}
+		}
+	}
 
 	return 0;
 }
