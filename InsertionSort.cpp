081a1b0c137ad7379e006297a7a426219e0a6086
blackjackals <65567021+blackjackals@users.noreply.github.com>
2020-10-04 11:44:08 +0700

    Update InsertionSort.cpp
    Adding a function for Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,8 +3,14 @@
 
 // Please add this function for insertion sort
 void insertionSort(int arr[], int n) {
-
-
+    for (int i = 1 ; i < n; i++) {
+        int currData = arr[i], j = i - 1;
+        while (j >= 0 && currData < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = currData;
+    }
 }
 
 int main() {
